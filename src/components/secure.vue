000ee2e4ea29c39d52b17cd65b6775d80e21d20d
1000copy <1000copy@gmail.com>
2020-03-17 17:20:40 +0800

    login 2

---
+++ src/components/secure.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="secure">
+        <h1>Secure Area</h1>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Secure',
+        data() {
+            return {};
+        }
+    }
+</script>
+
